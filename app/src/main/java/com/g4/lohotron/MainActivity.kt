af9ad58c852c7c1d1ff7bc8e00ec6cf2cde8312d
mi_g_alex <sashagorgun@yandex.com>
2022-04-18 08:55:30 +0300

    lohotron 2000

--- app/src/main/java/com/g4/lohotron/MainActivity.kt
+++ app/src/main/java/com/g4/lohotron/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.g4.lohotron
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
 import android.widget.Chronometer
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,18 +15,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val chronometer: Chronometer = findViewById(R.id.chronometer)
         var onWork : Boolean = false
-        val button: ImageButton = findViewById(R.id.button)
-        button.setOnClickListener {
-            if(!onWork){
-                chronometer.base = SystemClock.elapsedRealtime()
-                chronometer.start()
-                onWork = true
-            }
-            else {
-                chronometer.stop()
-                onWork = false
+        val buttonPlay: ImageButton = findViewById(R.id.buttonPlay)
+        val buttonPause : ImageButton = findViewById(R.id.buttonPause)
+        val buttonStop : ImageButton = findViewById(R.id.buttonStop)
 
-            }
+        buttonPlay.setOnClickListener{
+
+            chronometer.start()
+            onWork = true
+            buttonPause.isVisible = true
+            buttonStop.isVisible = true
+            buttonPlay.isVisible = false
+
         }
+
+        buttonPause.setOnClickListener{
+
+            chronometer.stop()
+            onWork = false
+            buttonPause.isVisible = false
+            buttonPlay.isVisible = true
+
+        }
+
+        buttonStop.setOnClickListener(){
+
+            chronometer.base = SystemClock.elapsedRealtime()
+            buttonPause.isVisible = false
+            buttonPlay.isVisible = true
+            buttonStop.isVisible = false
+            chronometer.stop()
+            onWork = false
+
+        }
+
+
     }
 }
